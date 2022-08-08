f59e618f5f3d6e49bf99f3fcc3966b6c4bb9b4a3
jess <jcottrill1123@gmail.com>
2022-08-08 11:29:23 -0400

    initial commit

---
+++ Methods.Exercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Methods.Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your Name?");
+            var name = Console.ReadLine();
+            Console.WriteLine(name);
+
+            Console.WriteLine("What is you favorite color?");
+            var color = Console.ReadLine();
+            Console.WriteLine(color);
+
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine();
+            Console.WriteLine(animal);
+
+            Console.WriteLine("What is your favorite band?");
+            var band = Console.ReadLine();
+            Console.WriteLine(band);
+
+        }
+    }
+}
